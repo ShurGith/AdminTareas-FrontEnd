c04346d5fd60bd2aec793cb5841c0608ba142e65
ShurGith <75266693+ShurGith@users.noreply.github.com>
2025-08-14 18:37:36 +0200

    Inicio

---
+++ src/layouts/AppLayout.tsx
@@ -0,0 +1,26 @@
+import { Outlet } from "react-router-dom";
+import Logo from "@/components/Logo";
+import NavMenu from "@/components/NavMenu";
+
+export default function AppLayout() {
+  return (
+    <>
+      <header className="bg-gray-800 py-5">
+        <div className="mx-auto max-w-screen-2xl flex flex-col lg:flex-row justify-between items-center gap-4 px-6">
+          <div className="w-64">
+            <Logo />
+          </div>
+          <NavMenu />
+        </div>
+      </header>
+      <section className="max-w-screen-2xl mt-10 py-5 px-30">
+      <Outlet />
+      </section>
+      <footer className="py-5 bg-gray-900">
+        <p className="text-center text-white">
+          © Admin Tareas - Todos los derechos reservados. - 2020 - {new Date().getFullYear()}
+        </p>
+      </footer>
+    </>
+  )
+}
---
+++ src/views/projects/CreateProjectView.tsx
@@ -0,0 +1,48 @@
+import { Link } from "react-router-dom";
+import { useForm } from 'react-hook-form';
+import ProjectForm from "@/components/ProjectForm";
+
+export default function CreateProjectView() {
+  function handleForm(data) {
+    console.log(data);
+  }
+
+  const initialValues = {
+    projectName: "",
+    clientName: "",
+    description: "",
+  };
+  const { register, handleSubmit, formState: { errors } } = useForm({ defaultValues: initialValues }) //register es una funcion que nos permite registrar los inputs del formulario y handleSubmit es una funcion que nos permite enviar el formulario. formState es un objeto que contiene la informacion de los campos del formulario. 
+  return (
+    <div className="container max-w-3xl mx-auto">
+      <h1 className="text-5xl font-black ">Mis Proyectos</h1>
+      <p className="text-2xl font-light text-gray-500 mt-5">
+        Llena el siguiente formulario para crear un nuevo proyecto.
+      </p>
+      <nav className="my-5">
+        <Link
+          to="/"
+          className="bg-purple-400 hover:bg-purple-500 px-10 py-3 text-white text-sm rounded-lg uppercase font-bold transition-colors">
+          Home Projectos
+        </Link>
+      </nav>
+
+      <form
+        className="mt-10 p-10 bg-white shadow-lg dark:bg-slate-900 rounded-lg"
+        onSubmit={handleSubmit(handleForm)}
+        noValidate
+        autoComplete='off'
+      >
+        <ProjectForm 
+          register={register}
+          errors={errors}
+        />
+        <input
+          type="submit"
+          value="Crear Proyecto"
+          className="mx-auto block bg-fuchsia-600 hover:bg-fuchsia-800 p-5 text-white uppercase font-bold cursor-pointer rounded-sm"
+        />
+      </form>
+    </div>
+  )
+}

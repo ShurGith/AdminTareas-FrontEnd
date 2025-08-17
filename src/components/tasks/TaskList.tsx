2b62736dc7c1b6214bc1e8ab77dd61a5e71938de
ShurGith <75266693+ShurGith@users.noreply.github.com>
2025-08-17 02:17:31 +0200

    Update TaskList.tsx

--- src/components/tasks/TaskList.tsx
+++ src/components/tasks/TaskList.tsx
@@ -44,7 +44,7 @@ export default function TaskList({ tasks }: TaskListProps) {
       <div className='flex gap-5 overflow-x-scroll 2xl:overflow-auto pb-32'>
         {Object.entries(groupedTasks).map(([status, tasks]) => (
           <div key={status} className='min-w-[300px] 2xl:min-w-0 2xl:w-1/5'>
-            <h3 className={`capitalize mb-4 text-lg font-light border-l-2 ${statusColors[status]} bg-white p-3 border-t-8`}>{statusTranslations[status]}</h3>
+            <h3 className={`capitalize text-lg font-light border ${statusColors[status]} bg-white p-3 border-t-8`}>{statusTranslations[status]}</h3>
             <ul className='mt-5 space-y-5'>
               {tasks.length === 0 ? (
                 <li className="text-gray-500 text-center pt-3">No Hay tareas</li>
